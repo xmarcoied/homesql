c8486268377674d7cfcb71ba291501549638f194
Marco Younan <myounan@cloudinn.net>
2020-02-17 17:59:34 +0200

    adding mocked database

---
+++ mocked.go
@@ -0,0 +1,22 @@
+package main
+
+import "context"
+
+type mockedDatabase struct{}
+
+var (
+	HomeStore  []Home
+	UsersStore []User
+)
+
+func (m mockedDatabase) GetUsers(ctx context.Context) ([]User, error) {
+	return UsersStore, nil
+}
+
+func (m mockedDatabase) NewUser(input User) (User, error) {
+	return input, nil
+}
+
+func (m mockedDatabase) NewHome(input Home) (Home, error) {
+	return input, nil
+}
